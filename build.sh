7b16238b0d483039d7b2f6e23bf0117f9646ec6e
Paul Clarke <paulsclarke2@gmail.com>
2026-07-27 09:12:51 -0500

    Update build.sh for new project files

--- build.sh
+++ build.sh
@@ -1,2 +1,20 @@
-#!/bin/bash
-gcc -g -o quickpick quickpick.c draw.c util.c glad.c profiler.c -Iinclude $(pkg-config --libs --cflags freetype2) $(pkg-config --libs --cflags sdl2) -lGL -lm # -framework OpenGL -lm
+#!/bin/sh
+set -eu
+cd "$(dirname "$0")"
+
+CC=${CC:-gcc}
+
+pkg-config --exists sdl3 freetype2 || {
+    echo "error: sdl3 and freetype2 development packages required" >&2
+    exit 1
+}
+
+${CC} -g ${CFLAGS:-} -o quickpick \
+    src/quickpick.c \
+    src/util/autil.c src/util/math.c src/util/string.c src/util/containers.c \
+    src/util/window_sdl.c src/util/window_common.c src/util/draw_opengl.c \
+    src/util/draw_common.c src/util/tlsf.c src/util/glad.c \
+    src/util/platform_unix.c \
+    -Iinclude -Isrc/util -Ires \
+    $(pkg-config --libs --cflags freetype2) $(pkg-config --libs --cflags sdl3) \
+    -lm ${LDFLAGS:-} "$@"
